af009be70deddd23f0ad96744f47e44d51060600
nikk434 <nskshirsagar434@gmail.com>
2025-04-16 17:12:57 +0530

    deleted unwanted loanBot

--- loanBot/base_state.py
+++
@@ -1,9 +0,0 @@
-class BaseState:
-    """Abstract base class for states."""
-
-    def __init__(self, state_manager):
-        self.state_manager = state_manager
-
-    def handle(self, user_message, phone_number):
-        """To be implemented by child states."""
-        raise NotImplementedError("Handle method must be implemented in child state classes.")
--- loanBot/state/loan/awaiting_loan_QA.py
+++
@@ -1,3 +0,0 @@
-class AwaitingLoanQA(BaseState):
-    def handle(self, user_message, phone_number)  
-    # Processes loan-related questions using Qdrant/CosmosDB.
