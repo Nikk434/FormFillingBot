ef8c0ff14d92f7803a28b247c91ea2e4d0cd67f9
Nikk434 <159627869+Nikk434@users.noreply.github.com>
2025-04-16 22:39:01 +0530

    Delete frontend directory

--- frontend/client/src/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-export const formatTimestamp = (timestamp) => {
-  if (!timestamp) return '';
-  
-  const date = new Date(timestamp);
-  return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-};
